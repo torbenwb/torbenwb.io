d4e23f9975234b5b28edd6dd3e1bf26a6e4aa63f
torbenwb <torbenwbernhard@gmail.com>
2023-12-23 12:02:13 -0800

    Trying to fix spa routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import { Home } from './pages/Home';
-import { About } from './pages/About';
-import { Projects } from './pages/Projects';
-import { Router, Route, Routes } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import { SiteNav } from './components/SiteNav';
+import logo from "./logo.svg";
+import "./App.css";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Projects } from "./pages/Projects";
+import { Router, Route, Routes, HashRouter } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { SiteNav } from "./components/SiteNav";
 
 function App() {
   return (
     <Container>
       <SiteNav />
       <Routes>
-        <Route path="/torbenwb.io/" element={<Home/>} />
-        <Route path="/torbenwb.io/about" element={<About />} />
-        <Route path="/torbenwb.io/projects" element={<Projects />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </Container>
-    
   );
 }
 
--- src/components/SiteNav.jsx
+++ src/components/SiteNav.jsx
@@ -1,17 +1,16 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
 
-export const SiteNav = () => 
-{
-    return (
-        <Navbar bg="light" data-bs-theme="light">
-            <Container>
-            <Navbar.Brand href="/torbenwb.io/">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-                <Nav.Link href="/torbenwb.io/">Home</Nav.Link>
-                <Nav.Link href="/torbenwb.io/about">About</Nav.Link>
-                <Nav.Link href="/torbenwb.io/projects">Projects</Nav.Link>
-            </Nav>
-            </Container>
-        </Navbar>
-    )
-}
+export const SiteNav = () => {
+  return (
+    <Navbar bg="light" data-bs-theme="light">
+      <Container>
+        <Navbar.Brand href="/torbenwb.io/">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/torbenwb.io/">Home</Nav.Link>
+          <Nav.Link href="/torbenwb.io/#/about">About</Nav.Link>
+          <Nav.Link href="/torbenwb.io/#/projects">Projects</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
